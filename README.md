e6bf16f2d2eeae944fd8226f96f09328c8394357
Tom Weatherhead <tomw@2hrd4u.org>
2022-09-22 14:01:43 -0600

    Initial repository population

--- README.md
+++ README.md
@@ -1,2 +1,147 @@
 # lobotomy
 A Brainf*ck interpreter written in C.
+
+(Pardon my language.)
+
+[![build status][build-status-badge-image]][build-status-url]
+[![latest tag][latest-tag-badge-image]][latest-tag-url]
+[![watchers][watchers-badge-image]][watchers-url]
+[![stars][stars-badge-image]][stars-url]
+[![issues][issues-badge-image]][issues-url]
+[![forks][forks-badge-image]][forks-url]
+[![contributors][contributors-badge-image]][contributors-url]
+[![branches][branches-badge-image]][branches-url]
+[![releases][releases-badge-image]][releases-url]
+[![commits][commits-badge-image]][commits-url]
+[![last commit][last-commit-badge-image]][last-commit-url]
+[![license][license-badge-image]][license-url]
+
+The brainf*ck language is a minimalistic imperative programming language, designed by Urban Müller around 1993.
+
+From [the language's Wikipedia page](https://en.wikipedia.org/wiki/Brainfuck#Language_design):
+
+The language consists of eight commands, listed below. A brainf*ck program is a sequence of these commands, possibly interspersed with other characters (which are ignored). The commands are executed sequentially, with some exceptions: an instruction pointer begins at the first command, and each command it points to is executed, after which it normally moves forward to the next command. The program terminates when the instruction pointer moves past the last command.
+
+The brainf*ck language uses a simple machine model consisting of the program and instruction pointer, as well as a one-dimensional array of at least 30,000 byte cells initialized to zero; a movable data pointer (initialized to point to the leftmost byte of the array); and two streams of bytes for input and output (most often connected to a keyboard and a monitor respectively, and using the ASCII character encoding).
+
+The eight language commands each consist of a single character:
+
+| Character | Meaning |
+|-----------|---------|
+| > | Increment the data pointer (to point to the next cell to the right) |
+| < | Decrement the data pointer (to point to the next cell to the left) |
+| + | Increment (increase by one) the byte at the data pointer |
+| - | Decrement (decrease by one) the byte at the data pointer |
+| . | Output the byte at the data pointer |
+| , | Accept one byte of input, storing its value in the byte at the data pointer |
+| [ | If the byte at the data pointer is zero, then instead of moving the instruction pointer forward to the next command, jump it forward to the command after the matching ] command |
+| ] | If the byte at the data pointer is nonzero, then instead of moving the instruction pointer forward to the next command, jump it back to the command after the matching [ command |
+
+In a syntactically correct brainf*ck program, each [ character is matched uniquely by a ] character later in the program. This matching is similar to the way ( and ) characters match up into pairs in an algebraic expression.
+
+An example of a "Hello, World!" program:
+
+```
+++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>
+---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.
+```
+
+## Installation and Invocation (C)
+
+(These instructions for the C implementation were tested on macOS)
+
+First, ensure that gcc and make are installed.
+
+After cloning this repo, cd into its directory and do this:
+
+```sh
+$ cd src/c
+$ make
+```
+
+Then pass in the path of a brainf*ck program file as a command-line argument:
+
+```sh
+$ ./brainfsck ../../examples/hello-world.bf
+```
+
+## Installation and Invocation (Typescript)
+
+After cloning this repo, cd into its directory and do this:
+
+```sh
+$ npm run f
+$ npm link
+```
+
+Now the interpreter can be invoked from the command line via the 'bfi' command.
+
+Example:
+
+```sh
+$ bfi examples/hello-world.bf
+```
+
+## About the Language
+
+A brainf*ck language reference can be found [here](http://brainfuck.org/brainfuck.html).
+
+For additional brainf*ck resources, please see:
+
+- Wikipedia page: [here](https://en.wikipedia.org/wiki/Brainfuck)
+- The language's page at esolangs.org (esoteric programming languages): [here](https://esolangs.org/wiki/Brainfuck)
+- Example source code, and more: [here](http://brainfuck.org/)
+- An online interpreter: [here](https://bf.doleczek.pl/)
+- An interpreter plus visualizer: [here](https://github.com/fatiherikli/brainfuck-visualizer)
+- An x86_64 compiler for Linux: [here](https://github.com/depsterr/bc)
+- A brainf*ck 'torture test': [here](https://github.com/rdebath/Brainfuck)
+
+## Notes to self
+
+TODO: When tests are added, add these devDependencies:
+
+```sh
+$ npm i -D @babel/core @babel/preset-env @babel/preset-typescript @types/jest jest
+```
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
+
+[build-status-badge-image]: https://circleci.com/gh/tom-weatherhead/lobotomy.svg?style=shield
+[build-status-url]: https://circleci.com/gh/tom-weatherhead/lobotomy
+[latest-tag-badge-image]: https://badgen.net/github/tag/tom-weatherhead/lobotomy
+[latest-tag-url]: https://github.com/tom-weatherhead/lobotomy/tags
+[watchers-badge-image]: https://badgen.net/github/watchers/tom-weatherhead/lobotomy
+[watchers-url]: https://github.com/tom-weatherhead/lobotomy/watchers
+[stars-badge-image]: https://badgen.net/github/stars/tom-weatherhead/lobotomy
+[stars-url]: https://github.com/tom-weatherhead/lobotomy/stargazers
+[issues-badge-image]: https://badgen.net/github/issues/tom-weatherhead/lobotomy
+[issues-url]: https://github.com/tom-weatherhead/lobotomy/issues
+[forks-badge-image]: https://badgen.net/github/forks/tom-weatherhead/lobotomy
+[forks-url]: https://github.com/tom-weatherhead/lobotomy/network/members
+[contributors-badge-image]: https://badgen.net/github/contributors/tom-weatherhead/lobotomy
+[contributors-url]: https://github.com/tom-weatherhead/lobotomy/graphs/contributors
+[branches-badge-image]: https://badgen.net/github/branches/tom-weatherhead/lobotomy
+[branches-url]: https://github.com/tom-weatherhead/lobotomy/branches
+[releases-badge-image]: https://badgen.net/github/releases/tom-weatherhead/lobotomy
+[releases-url]: https://github.com/tom-weatherhead/lobotomy/releases
+[commits-badge-image]: https://badgen.net/github/commits/tom-weatherhead/lobotomy
+[commits-url]: https://github.com/tom-weatherhead/lobotomy/commits/master
+[last-commit-badge-image]: https://badgen.net/github/last-commit/tom-weatherhead/lobotomy
+[last-commit-url]: https://github.com/tom-weatherhead/lobotomy
+[types-badge-image]: https://badgen.net/npm/types/lobotomy
+[types-url]: https://badgen.net/npm/types/lobotomy
+[install-size-badge-image]: https://badgen.net/packagephobia/install/lobotomy
+[install-size-url]: https://badgen.net/packagephobia/install/lobotomy
+[known-vulnerabilities-badge-image]: https://snyk.io/test/github/tom-weatherhead/lobotomy/badge.svg?targetFile=package.json&package-lock.json
+[known-vulnerabilities-url]: https://snyk.io/test/github/tom-weatherhead/lobotomy?targetFile=package.json&package-lock.json
+[lines-of-code-badge-image]: https://badgen.net/codeclimate/loc/tom-weatherhead/lobotomy
+[lines-of-code-url]: https://badgen.net/codeclimate/loc/tom-weatherhead/lobotomy
+[technical-debt-badge-image]: https://badgen.net/codeclimate/tech-debt/tom-weatherhead/lobotomy
+[technical-debt-url]: https://badgen.net/codeclimate/tech-debt/tom-weatherhead/lobotomy
+[maintainability-badge-image]: https://api.codeclimate.com/v1/badges/00000000000000000000/maintainability
+[maintainability-url]: https://codeclimate.com/github/tom-weatherhead/lobotomy/maintainability
+[test-coverage-badge-image]: https://api.codeclimate.com/v1/badges/00000000000000000000/test_coverage
+[test-coverage-url]: https://codeclimate.com/github/tom-weatherhead/lobotomy/test_coverage
+[license-badge-image]: https://img.shields.io/github/license/mashape/apistatus.svg
+[license-url]: https://github.com/tom-weatherhead/thaw-grammar/blob/master/LICENSE
---
+++ src/lobotomy.c
@@ -0,0 +1,327 @@
+/* lobotomy/src/c/lobotomy.c */
+/* ThAW: Started on 2022-08-15 */
+
+/* To compile and link: $ make */
+/* To remove all build products: $ make clean */
+
+/* A demonstration: $ make && ./lobotomy ../../examples/hello-world.bf */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/* #include <ctype.h> */
+/* #include <assert.h> */
+
+/* #if WINDOWS */
+/* #include <conio.h> / * For non-blocking getch() (without echo) and getche() (with echo) */
+/* #endif */
+
+/* #if MACOS or LINUX */
+#include <termios.h>
+#include <unistd.h>
+/* #endif */
+
+#if !defined(BOOL) && !defined(FALSE) && !defined(TRUE)
+/* Our poor man's Boolean data type: */
+#define BOOL int
+#define FALSE 0
+#define TRUE 1
+#endif
+
+const int maxProgramTapeLength = 4096;
+char * programTape = NULL;
+/* Assume that the program on the tape is terminated by a null character. */
+
+const int maxDataTapeLength = 4096;
+char * dataTape = NULL;
+
+const int maxNumSquareBracketPairIndices = 20;
+int numSquareBracketPairIndices = 0;
+int squareBracketPairIndices[2 * maxNumSquareBracketPairIndices];
+
+int leftSquareBracketIndices[maxNumSquareBracketPairIndices];
+
+BOOL calculateSquareBracketPairIndices(void) {
+	int i = 0;
+	int sp = 0;	/* Stack pointer; for the leftSquareBracketIndices */
+
+	numSquareBracketPairIndices = 0;
+
+	while (i < maxProgramTapeLength && programTape[i] != '\0') {
+
+		if (programTape[i] == '[') {
+
+			if (sp >= maxNumSquareBracketPairIndices) {
+				fprintf(stderr, "LeftSquareBracketIndices stack overflow.\n");
+				return FALSE;
+			}
+
+			leftSquareBracketIndices[sp++] = i;
+		} else if (programTape[i] == ']') {
+
+			if (numSquareBracketPairIndices >= maxNumSquareBracketPairIndices) {
+				fprintf(stderr, "SquareBracketPairIndices list overflow.\n");
+				return FALSE;
+			} else if (sp == 0) {
+				fprintf(stderr, "LeftSquareBracketIndices stack underflow; unmatched ].\n");
+				return FALSE;
+			}
+
+			squareBracketPairIndices[2 * numSquareBracketPairIndices] = leftSquareBracketIndices[--sp];
+			squareBracketPairIndices[2 * numSquareBracketPairIndices + 1] = i;
+			/* printf("SquareBracketPair %d: (%d, %d)\n", numSquareBracketPairIndices, squareBracketPairIndices[2 * numSquareBracketPairIndices], squareBracketPairIndices[2 * numSquareBracketPairIndices + 1]); */
+			++numSquareBracketPairIndices;
+		}
+
+		++i;
+	}
+
+	if (sp != 0) {
+		fprintf(stderr, "Unmatched [ remaining at end of program.\n");
+		return FALSE;
+	}
+
+	return TRUE;
+}
+
+int getMatchingBracketIndex(int i, int bracketIndex) {
+	int j;
+
+	for (j = 0; j < numSquareBracketPairIndices; ++j) {
+
+		if (squareBracketPairIndices[2 * j + bracketIndex] == i) {
+			return squareBracketPairIndices[2 * j + 1 - bracketIndex];
+		}
+	}
+
+	return -1; /* I.e. No match found */
+}
+
+/* #if MACOS or LINUX */
+/* From https://www.reddit.com/r/C_Programming/comments/v5k3z1/reading_char_for_char_from_stdin_without_waiting/ : */
+
+/* 2022-08-15 : This works on MacOS 12.5 with gcc: */
+
+int getch()  {
+	int ch;
+	struct termios oldattr, newattr;
+
+	tcgetattr(STDIN_FILENO, &oldattr);
+	newattr = oldattr;
+	newattr.c_lflag &= ~ICANON;
+	/* If the ECHO fplag is enabled, then we have implemented getche() rather than getch() */
+	newattr.c_lflag &= ~ECHO;
+	newattr.c_cc[VMIN] = 1;
+	newattr.c_cc[VTIME] = 0;
+	tcsetattr(STDIN_FILENO, TCSANOW, &newattr);
+	ch = getchar();
+	tcsetattr(STDIN_FILENO, TCSANOW, &oldattr);
+
+	return (ch == 4) ? EOF : ch;
+}
+/* #endif */
+
+BOOL readProgramFromFile(int argc, char * argv[]) {
+
+	if (argc < 2) {
+		fprintf(stderr, "readProgramFromFile() : Too few command-line arguments.\n");
+		return FALSE;
+	}
+
+	FILE * fp = fopen(argv[1], "r");
+	/* Then read all text from fp and store it in programTape. */
+
+	if (fp == NULL) {
+		fprintf(stderr, "readProgramFromFile() : Failed to open the file '%s'.\n", argv[1]);
+		return FALSE;
+	}
+
+	memset(programTape, 0, maxProgramTapeLength * sizeof(char));
+
+	int i;
+
+	for (i = 0; ; ++i) {
+		const int ch = getc(fp);
+
+		if (ch == EOF) {
+			break;
+		} else if (i >= maxProgramTapeLength) {
+			fprintf(stderr, "readProgramFromFile() : Program tape overflow\n");
+			fclose(fp);
+			return FALSE;
+		}
+
+		programTape[i] = (char)ch;
+	}
+
+	fclose(fp);
+	return TRUE;
+}
+
+int mainDelegate(int argc, char * argv[]) {
+	/* The Hello, World program:
+	strcpy(programTape, "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++."); */
+
+	/* The Echo program:
+	strcpy(programTape, ",[.,]"); */
+
+	/* The ... program:
+	strcpy(programTape, ""); */
+
+	if (!readProgramFromFile(argc, argv)) {
+		fprintf(stderr, "readProgramFromFile() failed.\n");
+		return 1; /* One (as a Unix exit code) indicates an error. */
+	}
+
+	if (!calculateSquareBracketPairIndices()) {
+		fprintf(stderr, "calculateSquareBracketPairIndices() failed.\n");
+		return 1;
+	}
+
+	printf("Starting the Brainfsck interpreter...\n\n");
+
+	/* printf("14 -> %d\n", getMatchingBracketIndex(14, 0));
+	printf("33 -> %d\n", getMatchingBracketIndex(33, 1));
+	printf("43 -> %d\n", getMatchingBracketIndex(43, 0));
+	printf("45 -> %d\n", getMatchingBracketIndex(45, 1));
+	printf("8 -> %d\n", getMatchingBracketIndex(8, 0));
+	printf("48 -> %d\n", getMatchingBracketIndex(48, 1));
+	printf("17 -> %d\n", getMatchingBracketIndex(17, 0));
+	printf("17 -> %d\n", getMatchingBracketIndex(17, 1)); */
+
+	int programTapeIndex = 0;
+	int dataTapeIndex = 0;
+
+	memset(dataTape, 0, maxDataTapeLength * sizeof(char));
+
+	/* let maxCharsToPrint = -1; // Set to a negative number to avoid limiting printed chars */
+
+	int valueFromGetchar = 0;
+
+	/* while (programTapeIndex < programTape.length && maxCharsToPrint !== 0) { */
+	while (programTapeIndex < maxProgramTapeLength && programTape[programTapeIndex] != '\0') {
+		const char ch = programTape[programTapeIndex++];
+
+		switch (ch) {
+			case '<': /* Move the pointer to the left */
+				if (--dataTapeIndex < 0) {
+					/* dataTape.unshift(0);
+					dataTapeIndex = 0; */
+					fprintf(stderr, "< : dataTapeIndex < 0\n");
+					return 1;
+					/* ? Or set dataTapeIndex = maxDataTapeLength - 1; ? */
+				}
+
+				break;
+
+			case '>': /* Move the pointer to the right */
+				if (++dataTapeIndex >= maxDataTapeLength) {
+					fprintf(stderr, "> : dataTapeIndex >= maxDataTapeLength\n");
+					return 1;
+					/* ? Or set dataTapeIndex = 0; ? (If we are not running in "strict mode") */
+				}
+
+				break;
+
+			case '+': /* Increment the memory cell at the pointer */
+				dataTape[dataTapeIndex]++;
+				break;
+
+			case '-': /* Decrement the memory cell at the pointer */
+				dataTape[dataTapeIndex]--;
+				break;
+
+			case ',': /* Input a character and store it in the cell at the pointer */
+				/* dataTape[dataTapeIndex] = (await getKeypress()).charCodeAt(0); */
+				/* getchar(); ? getch(); ? getc(); ? */
+
+				/* getchar() blocks execution until newline or EOF.
+				valueFromGetchar = getchar(); */
+
+				/* #if MACOS or LINUX */
+				valueFromGetchar = getch();
+				/* #endif */
+
+				/* getc(stdin) also blocks execution until newline or EOF.
+				valueFromGetchar = getc(stdin); */
+
+				if (valueFromGetchar == EOF) {
+					/* fprintf(stderr, "getchar() returned EOF\n");
+					return 1; */
+
+					/* Or just: */
+					valueFromGetchar = 0; /* Handle the EOF like a null char */
+					printf("getchar() returned EOF\n");
+				} else {
+					printf("getchar() returned char '%c', int %d\n", (char)valueFromGetchar, valueFromGetchar);
+				}
+
+				dataTape[dataTapeIndex] = (char)valueFromGetchar;
+				break;
+
+			case '.': /* Output the character signified by the cell at the pointer */
+				/* if (maxCharsToPrint > 0) {
+					maxCharsToPrint--;
+				} */
+
+				printf("%c", (char)dataTape[dataTapeIndex]);
+
+				break;
+
+			case '[': /* Jump past the matching ] if the cell at the pointer is 0 */
+				if (dataTape[dataTapeIndex] == 0) {
+					/* Find the index of the right bracket that matches this left bracket: */
+					programTapeIndex = getMatchingBracketIndex(
+						programTapeIndex,
+						0
+					);
+
+					if (programTapeIndex < 0) {
+						fprintf(stderr, "[ : Bad MatchingBracketIndex\n");
+						return 1;
+					}
+				}
+
+				break;
+
+			case ']': /* Jump back to the matching [ if the cell at the pointer is nonzero */
+				/* printf("] : programTapeIndex is %d; data at %d is %d\n", programTapeIndex, dataTapeIndex, (int)dataTape[dataTapeIndex]); */
+
+				if (dataTape[dataTapeIndex] != 0) {
+					/* Find the index of the left bracket that matches this right bracket: */
+					programTapeIndex = getMatchingBracketIndex(
+						programTapeIndex - 1,
+						1
+					);
+
+					if (programTapeIndex < 0) {
+						fprintf(stderr, "] : Bad MatchingBracketIndex\n");
+						return 1;
+					}
+				}
+
+				break;
+
+			default:
+				break;
+		}
+	}
+
+	return 0; /* Zero (as a Unix exit code) means success. */
+}
+
+int main(int argc, char * argv[]) {
+	programTape = (char *)malloc(maxProgramTapeLength * sizeof(char));
+	dataTape = (char *)malloc(maxDataTapeLength * sizeof(char));
+
+	const int status = mainDelegate(argc, argv);
+
+	free(dataTape);
+	free(programTape);
+
+	printf("\nThe program has terminated.\n\n");
+
+	return status;
+} /* main() */
+
+/* ******** End of brainfsck.c ******** */
